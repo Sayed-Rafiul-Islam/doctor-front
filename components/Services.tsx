3f894f8ac562bb5ea41336959aa146ef654faf70
Sayed Rafiul Islam <sayed80@student.sust.edu>
2024-03-14 03:56:47 +0600

    split

--- components/Services.tsx
+++ components/Services.tsx
@@ -7,12 +7,12 @@ import { useRouter } from "next/navigation";
 const Services = () => {
     const router = useRouter()
     return ( 
-        <div className="flex lg:flex-row md:flex-col-reverse flex-col-reverse w-5/6 items-center justify-center mx-auto mt-36
-         px-20 gap-10 bg-black bg-opacity-10 py-10 shadow-lg rounded-lg
+        <div className="flex lg:flex-row md:flex-col-reverse flex-col-reverse lg:w-5/6 w-11/12 items-center justify-center mx-auto mt-36
+         lg:px-20 px-5 gap-10 bg-black bg-opacity-10 py-10 shadow-lg rounded-lg
          overflow-hidden
          ">
             <div className="lg:w-1/3 flex flex-col justify-center">
-                <h2 className="lg:text-4xl lg:text-left md:text-2xl text-2xl text-center font-bold mb-4">Services I <span className="text-primary">Provide</span></h2>
+                <h2 className="lg:text-4xl lg:text-left md:text-xl text-2xl text-center font-bold mb-4">Services I <span className="text-primary">Provide</span></h2>
                 <p className="text-sm text-justify text-slate-800">As a cardiologist, I can provide a range of services related to the diagnosis, 
                 treatment, and management of cardiovascular conditions. Some of the services he can provide include
                 Cardiac Consultations, Diagnostic Testing, Treatment Planning, Management of Heart Conditions, 
